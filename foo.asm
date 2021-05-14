f9a6014eae8da687570ead1ecf798ef50ef8097e
Henning Ohlmeyer <henning.ohlmeyer@uni-muenster.de>
2021-05-14 22:52:40 +0200

    foo finished

--- foo.asm
+++ foo.asm
@@ -1,11 +1,36 @@
 # Verwendung der Register
 # $a0 .. Parameter Adresse des Arrays im Speicher
 # $a1 .. Parameter Anzahl der Elemente im Array
+# $t0 .. i
+# $t1 .. j
+# $t7 .. temporary storage for array values
+# $s0 .. temp
+# $s2 .. &a[i]
+# $s3 .. $a[j]
 # ... (weitere ergaenzen)
 
 foo:
     # Hier MIPS-Code ergaenzen
-    
+    add $t0, $zero, $zero # i = 0
+fori:
+    bge $t0, $a1, endfori
+    add $s0, $zero, $zero # temp = 0
+    move $t1, $t0 # j = i
+forj: 
+    bge $t1, $a1, endforj
+    sll $s3, $t1, 2
+    add $s3, $s3, $a0 # s3 = &a[j]
+    lw $t7, 0($s3) # load value of a[j]
+    add $s0, $s0, $t7 # temp += a[j]
+    addi $t1, $t1, 1 # j++
+    j forj
+endforj:
+    sll $s2, $t0, 2
+    add $s2, $s2, $a0 # s2 = &a[i]
+    sw $s0, 0($s2) # a[i] = temp
+    addi $t0, $t0, 1 # i++
+    j fori
+endfori:
     jr    $ra            # return (j=n in $v0)
 
 
